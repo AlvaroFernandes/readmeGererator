fa8231f591fe90b526a78bfa98345f58718fbda4
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-07-27 18:04:22 +1000

    fix some bugs on code.

--- readme_generated.md
+++ readme_generated.md
@@ -1,39 +1,38 @@
-# Project Title: asdasdasdasda,
+# Project Title: Readme Generator,
 
-    [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
 
-    This project was developed by [asdasdasd](https://github.com/asdasdasd).
-    
-    ## Table of Contents
-    ** [Description](#Description)
-    ** [Install Instructions](#Installation)
-    ** [License](#License)
-    ** [Usage](#Usage)
-    ** [Tests](#Tests)
-    ** [Contribution](#Contribution)
+  This project was developed by [AlvaroFernandes](https://github.com/AlvaroFernandes).
+  
+  ## Table of Contents
+  ** [Description](#Description)
+  ** [Install Instructions](#Installation)
+  ** [License](#License)
+  ** [Usage](#Usage)
+  ** [Tests](#Tests)
+  ** [Contribution](#Contribution)
 
-    
-    
-    # Description:
-    asdadasdasd
-    
-    # Installation instructions:
-    npm i
-    
-    # License:
-    This application is licensed under Mozilla Public License 2.0.
-    
-    # Usage:
-    dasdasdasdsa
+  
+  
+  # Description:
+  this is the Readme file generated using the readmeGenerator project.
+  
+  # Installation instructions:
+  npm i
+  
+  # License:
+  This application is licensed under Mozilla Public License 2.0.
+  
+  # Usage:
+  Create professional readme files for future projects
 
-    # Tests:
-    dasdasdasda
+  # Tests:
+  none
 
-    # Contribution:
-    dasdasdasdas
-    
-    # Questions
-    If any questions about the repo, contact me on asdasdasd. 
-    My GitHub profile is [Github profile](https://github.com/asdasdasd). 
-    
+  # Contribution:
+  none
+  
+  # Questions
+  If any questions about the repo, contact me on fernandes.alvaro@gmail.com. 
+  My GitHub profile is [Github profile](https://github.com/AlvaroFernandes). 
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,70 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
 
+    let typeLicense;
+
+    switch (answers.license) {
+        case 'MIT License':
+            typeLicense = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            break;
+        case 'GNU Lesser General Public License v3.0':
+            typeLicense = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+            break;
+        case 'Mozilla Public License 2.0':
+            typeLicense = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+            break;
+        case 'GNU Affero General Public License v3.0':
+            typeLicense = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+            break;
+        case 'The Unlicense':
+            typeLicense = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+            break;
+        case 'Apache License 2.0':
+            typeLicense = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            break;
+        case 'GNU General Public License v3.0':
+            typeLicense = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+            break;
+    }
+
+    return `# Project Title: ${answers.projectName},
+
+  ${typeLicense}
+
+  This project was developed by [${answers.userName}](https://github.com/${answers.userName}).
+  
+  ## Table of Contents
+  ** [Description](#Description)
+  ** [Install Instructions](#Installation)
+  ** [License](#License)
+  ** [Usage](#Usage)
+  ** [Tests](#Tests)
+  ** [Contribution](#Contribution)
+
+  
+  
+  # Description:
+  ${answers.description}
+  
+  # Installation instructions:
+  ${answers.install}
+  
+  # License:
+  This application is licensed under ${answers.license}.
+  
+  # Usage:
+  ${answers.usage}
+
+  # Tests:
+  ${answers.test}
+
+  # Contribution:
+  ${answers.contribution}
+  
+  # Questions
+  If any questions about the repo, contact me on ${answers.email}. 
+  My GitHub profile is [Github profile](https://github.com/${answers.userName}). 
+  
 `;
 }
 
